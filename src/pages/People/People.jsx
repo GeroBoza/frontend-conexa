2847529bc279056fc7e8764b2499dbd18de7e216
GeroBoza <geronimoboza@gmail.com>
2023-08-21 11:20:14 -0300

    fix People

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { DialogContentText, Grid, Typography } from "@mui/material";
+import { Avatar, DialogContentText, Grid, Typography } from "@mui/material";
 import { ApiService } from "../../services/ApiService";
 
 import ListLayout from "../ListLayout/ListLayout";
@@ -59,6 +59,17 @@ const People = () => {
         }
     };
 
+    function getPeopleId(url) {
+        const regex = /\/(\d+)\/$/;
+        const matches = url.match(regex);
+
+        if (matches && matches.length > 1) {
+            return parseInt(matches[1]);
+        }
+
+        return null;
+    }
+
     return (
         <ListLayout
             title={"People"}
@@ -104,15 +115,44 @@ const People = () => {
                     open={openModal}
                     handleClose={handleCloseModal}
                 >
-                    <DialogContentText>
-                        Birth year: <strong>{selectedItem.birth_year}</strong>
-                    </DialogContentText>
-                    <DialogContentText>
-                        Eye color: <strong>{selectedItem.eye_color}</strong>
-                    </DialogContentText>
-                    <DialogContentText>
-                        Skin color: <strong>{selectedItem.skin_color}</strong>
-                    </DialogContentText>
+                    <Grid
+                        container
+                        // display={"flex"}
+                        // justifyContent={"space-between"}
+                    >
+                        <Grid item xs={12} md={4}>
+                            <DialogContentText>
+                                Birth year:{" "}
+                                <strong>{selectedItem.birth_year}</strong>
+                            </DialogContentText>
+                            <DialogContentText>
+                                Eye color:{" "}
+                                <strong>{selectedItem.eye_color}</strong>
+                            </DialogContentText>
+                            <DialogContentText>
+                                Skin color:{" "}
+                                <strong>{selectedItem.skin_color}</strong>
+                            </DialogContentText>
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            md={8}
+                            display={"flex"}
+                            justifyContent={"center"}
+                        >
+                            <Avatar
+                                alt={selectedItem.name}
+                                src={`https://geroboza-bucket.s3.sa-east-1.amazonaws.com/sw/${getPeopleId(
+                                    selectedItem.url
+                                )}.png`}
+                                sx={{
+                                    width: 150,
+                                    height: 150,
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
                 </ListsModalContent>
             )}
         </ListLayout>
